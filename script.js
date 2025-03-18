07e1a0d5418f7d34dbfeb05e447158a456c03e61
soicrobbe <robbe.vanbiervliet@hotmail.com>
2025-03-18 22:57:08 +0100

    Project setup

---
+++ script.js
@@ -0,0 +1,8 @@
+let boekjaar = 0;
+let bedrijfsnummer = 0;
+let documentnummer = [0,0];
+let details = false;
+
+let demo = [{id: 1, BUKRS: 1111, GJAHR: 1111, BELNR: 1111111111, BLART: 11, BLDAT: '2025-03-18', BUZEI: 111, BSCHL: 11, AUGDT: '2025-03-18'}];
+
+console.log(demo[0].id);
